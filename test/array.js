4f45b71b600a738dd35fe55794fe78dad0efd627
Nathan Rajlich <nathan@tootallnate.net>
2012-09-15 23:01:39 -0700

    initial working implementation

--- test/array.js
+++ test/array.js
@@ -16,13 +16,17 @@ describe('Array', function () {
 
     it('should act like an Int32Array', function () {
       var Int32ArrayType = ArrayType('int32')
+      assert.equal(0, Int32ArrayType.size)
+
       var a = new Int32ArrayType(5)
+      assert.equal(5, a.length)
+      assert.equal(20, a.buffer.length)
       a[0] = 0
-      a[1] = 1
-      a[2] = 2
-      a[3] = 3
-
-
+      a[1] = 10
+      a[2] = 234
+      a[3] = 69
+      a[4] = 1410214
+      assert.deepEqual([0, 10, 234, 69, 1410214], a.toArray())
     })
 
   })
